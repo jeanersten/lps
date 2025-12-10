099b34833ddfe7813e9f448a9a4e891c49606b6a
Zaman Zulkarnaen <gavier.exe@gmail.com>
2025-12-10 14:14:45 +0700

    change GLM include name for file path consistency

--- Game/vendor/glm-1.0.2/source/glm.cpp
+++ Game/vendor/glm-1.0.2/source/glm.cpp
@@ -1,15 +1,12 @@
-/// @ref core
-/// @file glm/glm.cpp
-
 #ifndef GLM_ENABLE_EXPERIMENTAL
 #define GLM_ENABLE_EXPERIMENTAL
 #endif
-#include "GLM/gtx/dual_quaternion.hpp"
-#include "GLM/gtc/vec1.hpp"
-#include "GLM/gtc/quaternion.hpp"
-#include "GLM/ext/scalar_int_sized.hpp"
-#include "GLM/ext/scalar_uint_sized.hpp"
-#include "GLM/glm.hpp"
+#include "glm/gtx/dual_quaternion.hpp"
+#include "glm/gtc/vec1.hpp"
+#include "glm/gtc/quaternion.hpp"
+#include "glm/ext/scalar_int_sized.hpp"
+#include "glm/ext/scalar_uint_sized.hpp"
+#include "glm/glm.hpp"
 
 namespace glm
 {
